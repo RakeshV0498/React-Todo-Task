5003d88bfec2afd6b9fadb920990e72a862bf2c1
RakeshV0498 <rockr4674@gmail.com>
2024-04-14 13:56:51 +0530

    Created Jsx to display todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,27 @@ function App() {
   return (
     <>
       <TodoForm />
+      <div className="todo-header">
+        <h2 className="todo-title-text">My Todos</h2>
+        <form className="todo-filter">
+          <label
+            htmlFor="todo-filter"
+            className="todo-lbl"
+            id="todo-filter"
+          ></label>
+          <select name="filter" id="todo-filter" className="todo-select">
+            <option value="all" className="todo-filter-option">
+              All
+            </option>
+            <option value="completed" className="todo-filter-option">
+              Completed
+            </option>
+            <option value="not-completed" className="todo-filter-option">
+              Not-Completed
+            </option>
+          </select>
+        </form>
+      </div>
     </>
   );
 }
