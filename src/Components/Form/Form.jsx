5b65d1106f8141926de28bc2e81dd0039961e361
RakeshV0498 <rockr4674@gmail.com>
2024-04-14 19:13:44 +0530

    Implemented logic to add new todo

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import "./Form.css";
 
 const TodoForm = () => {
+  const [todoName, setTodoName] = useState("");
+  const [todoDescription, setTodoDescription] = useState("");
+
+  const handleNameChange = (e) => setTodoName(e.target.value);
+  const handleDescriptionChange = (e) => setTodoDescription(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the function passed from the parent component to add the new todo item
+    onAddTo({
+      name: todoName,
+      description: todoDescription,
+      status: false,
+    });
+    setTodoName("");
+    setTodoDescription("");
+  };
+
   return (
     <>
       <h1 className="title">My Todo</h1>
-      <form className="todo-form">
+      <form className="todo-form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="todo-input"
           required
           placeholder="Todo Name"
+          onChange={handleNameChange}
         />
         <input
           type="text"
           className="todo-input"
           required
           placeholder="Todo Description"
+          onChange={handleDescriptionChange}
         />
-        <button className="btn btn-submit">Add Todo</button>
+        <button type="submit" className="btn btn-submit">
+          Add Todo
+        </button>
       </form>
     </>
   );
