775cc4a6c321921807f03fb80813a501ba9b3c97
RakeshV0498 <rockr4674@gmail.com>
2024-04-14 17:29:51 +0530

    Added styles for Todo card container

--- src/Components/TodoCard/Card.jsx
+++ src/Components/TodoCard/Card.jsx
@@ -4,34 +4,36 @@ import "./Card.css";
 const TodoCard = ({ name, description, status = false }) => {
   return (
     <>
-      <h3 className="todo-name">Name: {name}</h3>
-      <p className="todo-desc">Description: {description}</p>
-      <div className="todo-status">
-        <label htmlFor="todo-filter" className="todo-lbl" id="todo-filter">
-          Status
-        </label>
-        <select name="filter" id="todo-filter" className="todo-select">
-          {status ? (
-            <>
-              {" "}
-              <option value="completed" className="todo-filter-option">
-                Completed
-              </option>
-              <option value="not-completed" className="todo-filter-option">
-                Not-Completed
-              </option>
-            </>
-          ) : (
-            <>
-              <option value="not-completed" className="todo-filter-option">
-                Not-Completed
-              </option>
-              <option value="completed" className="todo-filter-option">
-                Completed
-              </option>
-            </>
-          )}
-        </select>
+      <div className="todo-card">
+        <h3 className="todo-name">Name: {name}</h3>
+        <p className="todo-desc">Description: {description}</p>
+        <div className="todo-status">
+          <label htmlFor="todo-filter" className="todo-lbl" id="todo-filter">
+            Status
+          </label>
+          <select name="filter" id="todo-filter" className="todo-select">
+            {status ? (
+              <>
+                {" "}
+                <option value="completed" className="todo-filter-option">
+                  Completed
+                </option>
+                <option value="not-completed" className="todo-filter-option">
+                  Not-Completed
+                </option>
+              </>
+            ) : (
+              <>
+                <option value="not-completed" className="todo-filter-option">
+                  Not-Completed
+                </option>
+                <option value="completed" className="todo-filter-option">
+                  Completed
+                </option>
+              </>
+            )}
+          </select>
+        </div>
       </div>
     </>
   );
