6e0daf72cc2d7e0101e8bdfefea9d89ee8ae820a
RakeshV0498 <rockr4674@gmail.com>
2024-04-14 17:24:20 +0530

    Filter logic built for Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import TodoForm from "./Components/Form/Form";
+import TodoCard from "./Components/TodoCard/Card";
+
+const data = [
+  {
+    name: "My Todo -1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusamus nam itaque soluta iure.",
+    status: false,
+  },
+  {
+    name: "My Todo -2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusamus nam itaque soluta iure.",
+    status: true,
+  },
+];
+
 function App() {
+  const [filtervalue, setFilterValue] = useState("All");
+
+  const handleFilter = (e) => {
+    console.log(e.target.value);
+    setFilterValue(e.target.value);
+  };
   return (
     <>
       <TodoForm />
@@ -10,7 +34,12 @@ function App() {
           <label htmlFor="todo-filter" className="todo-lbl" id="todo-filter">
             Status Filter:
           </label>
-          <select name="filter" id="todo-filter" className="todo-select">
+          <select
+            name="filter"
+            id="todo-filter"
+            className="todo-select"
+            onChange={handleFilter}
+          >
             <option value="all" className="todo-filter-option">
               All
             </option>
@@ -22,6 +51,18 @@ function App() {
             </option>
           </select>
         </form>
+      </div>
+      <div className="todo-container">
+        {filtervalue === "all" &&
+          data.map((item, index) => <TodoCard key={index} {...item} />)}
+        {filtervalue === "completed" &&
+          data
+            .filter((item) => item.status === true)
+            .map((item, index) => <TodoCard key={index} {...item} />)}
+        {filtervalue === "not-completed" &&
+          data
+            .filter((item) => item.status === false)
+            .map((item, index) => <TodoCard key={index} {...item} />)}
       </div>
     </>
   );
