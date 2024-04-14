02b3ee4f98b7279ccf2bc993b3122263555f03db
RakeshV0498 <rockr4674@gmail.com>
2024-04-14 14:23:37 +0530

    Created Card Component

---
+++ src/Components/TodoCard/Card.jsx
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import "./Card.css";
+
+const TodoCard = ({ name, description, status = false }) => {
+  return (
+    <>
+      <h3 className="todo-name">Name: {name}</h3>
+      <p className="todo-desc">Description: {description}</p>
+      <div className="todo-status">
+        <label htmlFor="todo-filter" className="todo-lbl" id="todo-filter">
+          Status
+        </label>
+        <select name="filter" id="todo-filter" className="todo-select">
+          {status ? (
+            <>
+              {" "}
+              <option value="completed" className="todo-filter-option">
+                Completed
+              </option>
+              <option value="not-completed" className="todo-filter-option">
+                Not-Completed
+              </option>
+            </>
+          ) : (
+            <>
+              <option value="not-completed" className="todo-filter-option">
+                Not-Completed
+              </option>
+              <option value="completed" className="todo-filter-option">
+                Completed
+              </option>
+            </>
+          )}
+        </select>
+      </div>
+    </>
+  );
+};
+
+TodoCard.propTypes = {
+  name: PropTypes.string,
+  description: PropTypes.string,
+  status: PropTypes.bool,
+};
+
+export default TodoCard;
